bff4cf15c0c997cf65be84b68f53bc25e02b8e19
NguyenQuan <38609146+steve-gate@users.noreply.github.com>
2020-11-24 08:32:13 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const path = require('path');
 const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
+
+
 const app = express()
 const port = 3000
 
-app.get('/abcdef', (req, res) => {
-    res.send('Hello World!')
+app.use(express.static('src/public'))
+
+
+//Http logger
+app.use(morgan('combined'))
+//template engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'src/resources/views'));
+//
+
+
+//
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/new', (req, res) => {
+    res.render('new');
 })
 
 app.listen(port, () => {
